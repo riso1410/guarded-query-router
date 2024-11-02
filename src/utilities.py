b3c9b747ef6cfbe7862caaf33d732ee3af303d84
Riso <riso.sleher@gmail.com>
2024-11-03 00:25:33 +0100

    small updates setup for LSTM + CNN

--- src/utilities.py
+++ src/utilities.py
@@ -1,10 +1,16 @@
-import dotenv
+import torch
 
 class Config:
     """General configuration settings applicable across different models."""
 
-    def __init__(self, train_size=50, test_size=20, seed=22):
+    def __init__(self, train_size=50, test_size=20, max_sequence_length=100, vocab_size=10_000, embedding_dim=50, batch_size=32,seed=22):
         self.seed = seed
         self.train_size = train_size
         self.test_size = test_size
-        dotenv.load_dotenv()
+        self.max_sequence_length = max_sequence_length
+        self.vocab_size = vocab_size         
+        self.embedding_dim = embedding_dim        
+        self.batch_size = batch_size  
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        print(f"Device: {self.device}")
