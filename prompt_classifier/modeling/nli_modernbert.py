9d2b9b87145277a8156a8d03234b83be98039cbf
Riso <riso.sleher@gmail.com>
2025-02-23 19:31:49 +0100

    testing and fixes

--- prompt_classifier/modeling/nli_modernbert.py
+++ prompt_classifier/modeling/nli_modernbert.py
@@ -3,11 +3,12 @@ from transformers import pipeline
 
 class ModernBERTNLI:
     def __init__(self, domain: str) -> None:
+        self.domain = domain
         self.classifier = pipeline("zero-shot-classification", model="tasksource/ModernBERT-base-nli", device=0, truncation=True, max_length=256, batch_size=32)
-        self.label = [domain, "general"]
+        self.label = [f"related to {domain} ", f"not related to {domain}"]
 
     def predict(self, text: str) -> dict:
         prediction = self.classifier(text, self.label)
-        if prediction == "general":
+        if prediction == f"not related to {self.domain}":
             return 0
         return 1
